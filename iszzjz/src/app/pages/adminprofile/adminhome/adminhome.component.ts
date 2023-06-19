c7d6a8e2f9aea0ab190ca2d06fff2c92989498ed
dinela <dinelaganovic99@gmail.com>
2023-06-19 15:04:54 +0200

    finish authetication and authorization

--- iszzjz/src/app/pages/adminprofile/adminhome/adminhome.component.ts
+++ iszzjz/src/app/pages/adminprofile/adminhome/adminhome.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import {Input} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserstoreService } from 'src/app/services/userstore.service';
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
@@ -10,10 +10,19 @@ interface SideNavToggle {
   templateUrl: './adminhome.component.html',
   styleUrls: ['./adminhome.component.css']
 })
-export class AdminhomeComponent {
+export class AdminhomeComponent implements OnInit {
   isSideNavCollapsed = false;
   screenWidth = 0;
-
+  public role:string;
+  constructor( private auth: AuthService, private ustore: UserstoreService){
+  }
+  ngOnInit(): void {
+    this.ustore.getRoleFromStore()
+  .subscribe(val=>{
+    const role= this.auth.getRoleFromToken();
+    this.role=val|| role;
+  })
+  }
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
     this.isSideNavCollapsed = data.collapsed;
--- iszzjz/src/app/pages/adminrprofile/adminrhome/adminrhome.component.ts
+++ iszzjz/src/app/pages/adminrprofile/adminrhome/adminrhome.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserstoreService } from 'src/app/services/userstore.service';
 interface SideNavToggle {
   screenWidth: number;
   collapsed: boolean;
@@ -9,10 +10,19 @@ interface SideNavToggle {
   templateUrl: './adminrhome.component.html',
   styleUrls: ['./adminrhome.component.css']
 })
-export class AdminrhomeComponent {
+export class AdminrhomeComponent implements OnInit {
   isSideNavCollapsed = false;
   screenWidth = 0;
-
+  public role:string;
+constructor(private auth: AuthService, private ustore: UserstoreService){
+}
+ngOnInit(): void {
+  this.ustore.getRoleFromStore()
+  .subscribe(val=>{
+    const role= this.auth.getRoleFromToken();
+    this.role=val|| role;
+  })
+}
   onToggleSideNav(data: SideNavToggle): void {
     this.screenWidth = data.screenWidth;
     this.isSideNavCollapsed = data.collapsed;
