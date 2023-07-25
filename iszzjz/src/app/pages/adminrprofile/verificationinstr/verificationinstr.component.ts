552288ff573843e2bcf62bdadb48665c8e18371c
Minela <minelag556@gmail.com>
2023-07-25 15:11:36 +0200

    change table data

--- iszzjz/src/app/pages/adminrprofile/verificationinstr/verificationinstr.component.ts
+++ iszzjz/src/app/pages/adminrprofile/verificationinstr/verificationinstr.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-verificationinstr',
   templateUrl: './verificationinstr.component.html',
   styleUrls: ['./verificationinstr.component.css']
 })
-export class VerificationinstrComponent {
-
+export class VerificationinstrComponent implements OnInit {
+  public requests: any=[];
+  constructor( private api: ApiService){}
+  ngOnInit(): void {
+    this.api.getReguests()
+    .subscribe(res=>
+      {
+        this.requests=res;
+      })
+  }
+  public createImgPath = (serverPath: string) => { 
+    return `https://localhost:7059/${serverPath}`; 
+  }
 }
